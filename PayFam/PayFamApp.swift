9aab247aec6055699ddf40a6d6ae8fc69a3e79c2
matt-kay <kayodematthew56@gmail.com>
2022-11-28 05:17:38 +0100

    Initial Commit

---
+++ PayFam/PayFamApp.swift
@@ -0,0 +1,17 @@
+//
+//  PayFamApp.swift
+//  PayFam
+//
+//  Created by Mattosha on 28/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct PayFamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
